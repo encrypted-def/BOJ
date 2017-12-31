8cc8856b87f343154fc87c7fb04f2ceb98e963fd
blisstoner <blisstoner@gmail.com>
2017-12-31 23:35:44 +0900

    [BOJ] 2444번: 별찍기 - 7
    간단한 구현

---
+++ 2444.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void blank(int n) {
+    while (n--) printf(" ");
+}
+void star(int n) {
+    while (n--) printf("*");
+}
+int main(void) {
+    int N;
+    scanf("%d", &N);
+    for (int h = 1; h <= N; h++) {
+        blank(N - h);
+        star(2 * h - 1);
+        printf("\n");
+    }
+    for (int h = N - 1; h >= 1; h--) {
+        blank(N - h);
+        star(2 * h - 1);
+        printf("\n");
+    }
+}
