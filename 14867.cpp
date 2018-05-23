820ae341ddf401c9f7625cc4ef985d74bcdb0356
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-05-23 11:28:49 +0900

    [BOJ] 14867번: 물통
    Math, Implementation

---
+++ 14867.cpp
@@ -0,0 +1,190 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <functional>
+#include <iostream>
+#include <tuple>
+#include <string>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll MOD = 1000000000;
+ll POW(ll a, ll b, ll MMM = MOD) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a * (b / GCD(a, b)); }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+ll EXGCD(ll a, ll b, ll&x, ll&y) {
+	if (!b)return x = 1, y = 0, a;
+	ll d = EXGCD(b, a%b, x, y), t = x;
+	return x = y, y = t - a / b * y, d;
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+ll a, b, c, d;
+int D[1000004];
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	sf4(a, b, c, d);
+	if (c == 0 && d == 0) {
+		pf1(0);
+		return 0;
+	}
+	if (c == 0 && d == b) {
+		pf1(1);
+		return 0;
+	}
+	if (c == a && d == 0) {
+		pf1(1);
+		return 0;
+	}
+	if (c == a && d == b) {
+		pf1(2);
+		return 0;
+	}
+	if ((c != 0 && c != a) && (d != 0 && d != b)) {
+		pf1(-1);
+		return 0;
+	}
+	ll g = GCD(a, b);
+	if (c % g != 0 || d % g != 0) {
+		pf1(-1);
+		return 0;
+	}
+	ll x, y;
+	int cnt1 = 0;
+	int cnt2 = 0;
+	ll c1 = 0, d1 = 0;
+	// a에 채우고 b으로 옮기기
+	while (c1 != c || d1 != d) {
+		c1 = a;
+		cnt1++;
+		if (c1 == c && d1 == d) break;
+		c1 = 0;
+		d1 = d1 + a;
+		cnt1++;
+		bool over = false;
+		if (d1 > b) {
+			c1 = d1 - b; d1 = b; over = true;
+		}
+		if (c1 == c && d1 == d) break;
+		if (over) {
+			cnt1++;
+			d1 = 0;
+			if (c1 == c && d1 == d) break;
+			while (c1 > b) {
+				cnt1++;
+				d1 = b;
+				c1 -= b;
+				if (c1 == c && d1 == d) break;
+				cnt1++;
+				d1 = 0;
+				if (c1 == c && d1 == d) break;
+			}
+			if (c1 == c && d1 == d) break;
+			cnt1++;
+			d1 = c1;
+			c1 = 0;
+			if (c1 == c && d1 == d) break;
+		}
+	}
+	c1 = 0; d1 = 0;
+	swap(c, d);
+	swap(a, b);
+	// b에 채우고 a으로 옮기기
+	while (c1 != c || d1 != d) {
+		c1 = a;
+		cnt2++;
+		if (c1 == c && d1 == d) break;
+		c1 = 0;
+		d1 = d1 + a;
+		cnt2++;
+		bool over = false;
+		if (d1 > b) {
+			c1 = d1 - b; d1 = b; over = true;
+		}
+		if (c1 == c && d1 == d) break;
+		if (over) {
+			cnt2++;
+			d1 = 0;
+			if (c1 == c && d1 == d) break;
+			while (c1 > b) {
+				cnt2++;
+				d1 = b;
+				c1 -= b;
+				if (c1 == c && d1 == d) break;
+				cnt2++;
+				d1 = 0;
+				if (c1 == c && d1 == d) break;
+			}
+			if (c1 == c && d1 == d) break;
+			cnt2++;
+			d1 = c1;
+			c1 = 0;
+			if (c1 == c && d1 == d) break;
+		}
+	}
+	pf1(min(cnt1, cnt2));
+}
