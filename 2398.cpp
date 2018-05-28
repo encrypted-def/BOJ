0db9d6d36e6d126a72f1e39b7fe790140c4ed15c
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-05-28 20:20:23 +0900

    [BOJ] 2398번: Conference Call
    Floyd-Warshall Algorithm

---
+++ 2398.cpp
@@ -0,0 +1,128 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll MOD = 1000000000;
+ll POW(ll a, ll b, ll MMM = MOD) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a * (b / GCD(a, b)); }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+int N, M;
+int dist[1003][1003];
+int track[1003][1003];
+vector < pii > path;
+int s1, s2, s3;
+void route(int st, int en) {
+	if (st == en) return;
+	if (track[st][en] == 0) {
+		path.push_back({ st, en });
+		return;
+	}
+	route(st, track[st][en]);
+	route(track[st][en], en);
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	sf2(N, M);
+	rep(i, 1, N + 1) {
+		rep(j, 1, N + 1)
+			dist[i][j] = 0x7f7f7f;
+		dist[i][i] = 0;
+	}
+	while (M--) {
+		int a, b, c;
+		sf3(a, b, c);
+		dist[a][b] = c;
+		dist[b][a] = c;
+	}
+	sf3(s1, s2, s3);
+	rep(k, 1, N + 1) {
+		rep(i, 1, N + 1) {
+			rep(j, 1, N + 1) {
+				if (dist[i][j] > dist[i][k] + dist[k][j]) {
+					dist[i][j] = dist[i][k] + dist[k][j];
+					track[i][j] = k;
+				}
+			}
+		}
+	}
+	// s1 -> s2, s1 -> s3일 때, 중간 지점 t를 생각해서 d[s1][t]+d[t][s2]+d[t][s3]의 최솟값을 구하면 됨
+	int t = 1;
+	rep(i, 1, N + 1)
+		if (dist[s1][t] + dist[s2][t] + dist[s3][t] > dist[s1][i] + dist[s2][i] + dist[s3][i]) t = i;
+	route(s1, t);
+	route(s2, t);
+	route(s3, t);
+	pf2l(dist[s1][t] + dist[s2][t] + dist[s3][t], path.size());
+	for (auto const& p : path)
+		pf2l(p.X, p.Y);
+}
