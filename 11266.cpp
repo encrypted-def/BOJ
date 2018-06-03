de35c3eb39d2f4fd641d00cc038c9926d6d6e00b
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-06-03 22:33:45 +0900

    [BOJ] 11266번: 단절점
    DFS, graph theory

---
+++ 11266.cpp
@@ -0,0 +1,125 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll MOD = 1000000000;
+ll POW(ll a, ll b, ll MMM = MOD) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a * (b / GCD(a, b)); }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+
+// cur의 자식들 중에서 lowest의 min값을 반환
+int dfs(int cur, bool isRoot, vi& ret, vi& visitorder, vector<vi >& adj, int* cnt) {
+	bool isCutVertex = false;
+	int ans = *cnt;
+	visitorder[cur] = (*cnt)++;
+	int child = 0;
+	for (auto next : adj[cur]) {
+		if (visitorder[next] == 0) { // 아직 방문하지 않았다면
+			child++;
+			int next_lowest = dfs(next, false, ret, visitorder, adj, cnt);
+			if (!isRoot && next_lowest >= visitorder[cur]) isCutVertex = true;
+			if(ans > next_lowest) ans = next_lowest;
+		}
+		else
+			if (ans > visitorder[next]) ans = visitorder[next];
+	}
+	if (isRoot) isCutVertex = (child >= 2);
+	if (isCutVertex) ret.pb(cur);
+	return ans;
+}
+vi ArticulationPoint(int V, vector< vi >& adj) {
+	vi ret;
+	vi visitorder(V + 1);
+	int cnt = 1;
+	rep(i, 1, V + 1) {
+		if (visitorder[i] == 0)
+			dfs(i, true, ret, visitorder, adj, &cnt);
+	}
+	return ret;
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);	
+	int V, E;
+	sf2(V, E);
+	vector< vector<int> > adj(V+1);
+	while (E--) {
+		int a, b;
+		sf2(a, b);
+		adj[a].pb(b);
+		adj[b].pb(a);
+	}
+	vi CutVertex = ArticulationPoint(V, adj);
+	sort(all(CutVertex));
+	pf1l(CutVertex.size());
+	for (auto c : CutVertex) pf1(c);
+}
