8babfa6c50eb91dea415234ebc48616224cb3cf9
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-06-04 01:19:31 +0900

    [BOJ] 5866번: Meet and Greet
    Implementation

---
+++ 5866.cpp
@@ -0,0 +1,131 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll MOD = 1000000000;
+ll POW(ll a, ll b, ll MMM = MOD) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a * (b / GCD(a, b)); }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+
+
+int main(void) {
+	int B, E;
+	sf2(B, E);
+	vi Bmove(B + 1);
+	vi Emove(E + 1);
+	int tmp1;
+	char tmp2;
+	rep(i, 0, B){
+		sf2(tmp1,tmp2);
+		if (tmp2 == 'L') Bmove[i] = -tmp1;
+		else Bmove[i] = tmp1;
+	}
+	rep(i, 0, E) {
+		sf2(tmp1, tmp2);
+		if (tmp2 == 'L') Emove[i] = -tmp1;
+		else Emove[i] = tmp1;
+	}
+	int cnt = 0;
+	int Bpos = 0;
+	int Epos = 0;
+	int Bidx = 0;
+	int Eidx = 0;
+	while (Bidx < B || Eidx < E) {
+		bool signflag = false;
+		if (Bidx == B || Eidx == E) signflag = true; // 어느 하나가 끝에 도달했을 때
+		else if ((Bmove[Bidx] ^ Emove[Eidx]) & 0x80000000) signflag = true;
+		if (Eidx < E) {
+			if (Emove[Eidx] < 0) {
+				Epos--;
+				Emove[Eidx]++;
+			}
+			else {
+				Epos++;
+				Emove[Eidx]--;
+			}
+			if (Emove[Eidx] == 0) Eidx++;
+		}
+		if (Bidx < B) {
+			if (Bmove[Bidx] < 0) {
+				Bpos--;
+				Bmove[Bidx]++;
+			}
+			else {
+				Bpos++;
+				Bmove[Bidx]--;
+			}
+			if (Bmove[Bidx] == 0) Bidx++;
+		}
+		cnt+=(signflag && Epos == Bpos);
+	}
+	pf1(cnt);
+}
