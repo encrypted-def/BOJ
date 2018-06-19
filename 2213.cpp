5991829047ff77471a12fa14f6662414a32d5b22
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-06-20 05:36:07 +0900

    [BOJ] 2213번: 트리의 독립집합

---
+++ 2213.cpp
@@ -0,0 +1,135 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a,b) * b; }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(a) ((int)(a.size()))
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+
+int N;
+int W[10005];
+int D1[10005]; // D1[x] : x를 root로 하는 subtree에서 x를 포함한 최대독립집합의 크기
+int D2[10005]; // D2[x] : x를 root로 하는 subtree에서 x를 포함하지 않는 최대독립집합의 크기
+int choice[10005];
+vi adj[10005];
+vi adj_tree[10005];
+vi ans;
+int isVisited[10005];
+void dfs(int cur) {
+	D1[cur] = W[cur];
+	for (auto next : adj[cur]) {
+		if (isVisited[next]) continue;
+		isVisited[next] = true;
+		adj_tree[cur].pb(next);
+		dfs(next);
+		D1[cur] += D2[next];
+		D2[cur] += max(D1[next],D2[next]);
+		if (D1[next] > D2[next]) choice[next] = 1;
+		else choice[next] = 2;
+	}
+
+}
+
+void IndVertex(int cur, int ch) {
+	if (ch == 1)
+	{
+		ans.pb(cur);
+		for (auto next : adj_tree[cur]) {
+			IndVertex(next, 2);
+		}
+	}
+	else {
+		for(auto next : adj_tree[cur])
+			IndVertex(next, choice[next]);
+	}
+}
+
+int main(void) {
+	sf1(N);
+	rep(i, 0, N) sf1(W[i+1]);
+	rep(i, 0, N - 1) {
+		int a, b;
+		sf2(a, b);
+		adj[a].pb(b);
+		adj[b].pb(a);
+	}
+	isVisited[1] = 1;
+	dfs(1);
+	if (D1[1] > D2[1])
+		IndVertex(1, 1);
+	else
+		IndVertex(1, 2);
+	sort(all(ans));
+	pf1l(max(D1[1], D2[1]));
+	for (auto a : ans) pf1(a);
+}
