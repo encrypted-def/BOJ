f4d060431d9ca7a2ec4e93a7390c3414795a0e96
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-07-06 20:29:35 +0900

    [BOJ] 14428번: 수열과 쿼리 16
    Segment Tree

---
+++ 14428.cpp
@@ -0,0 +1,130 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+const int dx[4] = { 1,-1,0,0 };
+const int dy[4] = { 0,0,1,-1 };
+const int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+const int INF = 0x7f7f7f7f;
+const ll INF_LL = 0x7f7f7f7f7f7f7f7f;
+ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a, b) * b; }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(a) ((int)(a.size()))
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+
+const int MX_N = 100004;
+int seg[4 * MX_N];
+int arr[MX_N];
+const int INVALID = -1;
+
+int seg_func(int a, int b) { // min, xor, gcd, max, add, ...
+	if (a == INVALID) return b;
+	if (b == INVALID) return a;
+	return arr[a] <= arr[b] ? a : b;
+}
+//seg_update(i,val,1,1,N)
+int seg_update(int i, int val, int node, int st, int en) {
+	if (st > i || i > en) // i번째 값을 입력받았는데 node_st~node_en 범위 안에 i가 없을 경우
+		return seg[node];
+	if (st == en) // leaf에 도달한 경우
+		return seg[node] = i;
+	int mid = (st + en) / 2;
+	return seg[node] = seg_func(seg_update(i, val, node * 2, st, mid), seg_update(i, val, node * 2 + 1, mid + 1, en));
+}
+//seg_query(a,b,1,1,N)
+int seg_query(int a, int b, int node, int st, int en) {
+	if (en < a || b < st) // a~b와 node_st~node_en 범위가 아예 겹치지 않는 경우
+		return INVALID; // invalid value
+	if (a <= st && en <= b) // node_st~node_en 범위가 a~b 범위 안에 완벽하게 들어가는 경우
+		return seg[node];
+	int mid = (st + en) / 2;
+	return seg_func(seg_query(a, b, node * 2, st, mid), seg_query(a, b, node * 2 + 1, mid + 1, en));
+}
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int N;
+	sf1(N);
+	rep(i, 1, N + 1) {
+		sf1(arr[i]);
+		seg_update(i, arr[i], 1, 1, N);
+	}
+	int M;
+	sf1(M);
+	while (M--) {
+		int a, b,c;
+		sf3(a, b,c);
+		if (a == 1) {
+			arr[b] = c;
+			seg_update(b, c, 1, 1, N);
+		}
+		else
+			pf1l(seg_query(b, c, 1, 1, N));
+	}
+}
