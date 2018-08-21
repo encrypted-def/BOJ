1bf155114e52a4c2d069c6f81cc45b699790cfa2
blisstoner <20028331+blisstoner@users.noreply.github.com>
2018-08-21 23:19:29 +0900

    [BOJ] 1600번: 말이 되고픈 원숭이
    BFS

---
+++ 1600.cpp
@@ -0,0 +1,131 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>ordered_set;
+
+const int dx[4] = { 1,-1,0,0 };
+const int dy[4] = { 0,0,1,-1 };
+const int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+const int INF = 0x7f7f7f7f;
+const ll INF_LL = 0x7f7f7f7f7f7f7f7f;
+ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a, b) * b; }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(a) ((int)(a.size()))
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+int D[205][205][30]; // D[i][j][k] : min movement s.t. using knight's move k time
+int board[205][205];
+int kx[8]={-2,-1,1,2,-2,-1,1,2};
+int ky[8]={1,2,2,1,-1,-2,-2,-1};
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int K;
+	sf1(K);
+	int N,M;
+    sf2(M,N);
+    rep(i,0,N) rep(j,0,M) sf1(board[i][j]);
+    // dist1
+    rep(i,0,N) rep(j,0,M) rep(k,0,K+1) D[i][j][k] = -1;
+    D[0][0][0] = 0;
+    // bfs first
+    queue<ti3> Q;
+    Q.push({0,0,0});
+    while(!Q.empty()){
+        ti3 cur = Q.front();
+        Q.pop();
+        int x,y,k;
+        tie(x,y,k)=cur;
+        rep(dir,0,4){
+            int nx = x+dx[dir];
+            int ny = y+dy[dir];
+            if(OOB(nx,ny,N,M) || board[nx][ny] != 0 ||D[nx][ny][k] != -1) continue;
+            D[nx][ny][k] = D[x][y][k]+1;
+            Q.push({nx,ny,k});
+        }
+        if(k != K){
+            rep(dir,0,8){
+               int nx = x+kx[dir];
+               int ny = y+ky[dir];
+               if(OOB(nx,ny,N,M) || board[nx][ny] != 0 || D[nx][ny][k+1] != -1) continue;
+               D[nx][ny][k+1] = D[x][y][k]+1;
+               Q.push({nx,ny,k+1});
+            }
+        }
+    }
+    int mn = INF;
+    rep(k,0,K+1){
+        if(D[N-1][M-1][k]==-1) continue;
+        mn = min(mn,D[N-1][M-1][k]);
+    }
+    if(mn==INF) mn =-1;
+    pf1l(mn);
+}
