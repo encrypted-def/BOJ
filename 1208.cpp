c8d73527faeb8aa5896d848810a904a28484243c
blisstoner <blisstoner@gmail.com>
2018-12-18 16:02:30 +0900

    [BOJ] 1208번: 부분집합의 합 2
    Meet in the middle

---
+++ 1208.cpp
@@ -0,0 +1,120 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+typedef unsigned int ui;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>ordered_set;
+
+const int dx[4] = { 1,0,-1,0 };
+const int dy[4] = { 0,1,0,-1 };
+const int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a, b) * b; }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(a) ((int)(a.size()))
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+
+int n,s;
+int a[45];
+int D1[1<<20];
+int D2[1<<20];
+const int pad = 4000000;
+int occur1[2*pad+1];
+int occur2[2*pad+1];
+
+map<int,int> m1,m2;
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+  sf2(n,s);
+  rep(i,0,n) sf1(a[i]);
+  rep(bitmask, 1, (1<<(n/2)) ){
+    int bit1 = bitmask & -bitmask;
+    D1[bitmask] = D1[bitmask ^ bit1] + a[31-__builtin_clz(bit1)];
+    occur1[pad+D1[bitmask]]++;
+  }
+  
+  int Sz = n-n/2;
+  rep(bitmask,1, (1<<Sz)){
+    int bit1 = bitmask & -bitmask;
+    D2[bitmask] = D2[bitmask ^ bit1] + a[n/2+31-__builtin_clz(bit1)];
+    occur2[pad+D2[bitmask]]++;
+  }
+  ll ans = occur1[s+pad]+occur2[s+pad];
+  // (i-pad)+t = s -> t =  s - i + pad
+  rep(i,0,2*pad+1){
+    if(s-i+2*pad < 0 or s-i+2*pad > 2*pad) continue;
+    ans += (ll)occur1[i]*occur2[s-i+2*pad];
+  }
+  pf1l(ans);
+}
