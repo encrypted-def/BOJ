d124c641fb1cd6fc8c36b6988d25124cb737bd05
blisstoner <blisstoner@gmail.com>
2019-02-05 22:20:11 +0900

    [BOJ] 16119번: Cherrypick
    Dynamic Programming

---
+++ 16119.cpp
@@ -0,0 +1,116 @@
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+typedef unsigned int ui;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>ordered_set;
+
+const int dx[4] = { 1,0,-1,0 };
+const int dy[4] = { 0,1,0,-1 };
+const int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a, b) * b; }
+ll INV(ll a, ll m) {
+	ll m0 = m, y = 0, x = 1;
+	if (m == 1)	return 0;
+	while (a > 1) {
+		ll q = a / m;
+		ll t = m;
+		m = a % m, a = t;
+		t = y;
+		y = x - q * y;
+		x = t;
+	}
+	if (x < 0) x += m0;
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
+#define X first
+#define Y second
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(a) ((int)(a.size()))
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+
+int n;
+const int pad = 72;
+int c[1000+2*pad][1000+2*pad];
+int d[pad][1000+2*pad][1000+2*pad];
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+  sf1(n);
+  rep(i,pad,n+pad) rep(j,pad,n+pad){ sf1(c[i][j]); d[1][i][j] = c[i][j];}
+  rep(t,2,pad){
+    rep(i,t-1,1000+2*pad){
+      rep(j,t-1,1000+2*pad){
+        d[t][i][j] = max(d[t-1][i-1][j],d[t-1][i][j-1]);
+        d[t][i][j] = max(d[t][i][j], c[i-t+1][j-t+1]);
+        d[t][i][j] = max(d[t][i][j], c[i][j]);                
+      }
+    }    
+  }
+  rep(i,pad,n+pad){
+    rep(j,pad,n+pad){
+      int val = d[1][i][j]-1;
+      for(int k = 3; k <= 69; k+=2){
+        val = max(val,d[k][i+k/2][j+k/2]-(k/2+1)*(k/2+1));
+      //  if(i==pad and j==pad) cout << d[k][i+k/2][j+k/2] << ' ' << '\n';
+      }
+      cout << val << ' ';
+    }
+    cout << '\n';
+  }
+}
