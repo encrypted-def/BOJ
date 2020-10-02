5a950ff7b4cc74a4ae9d016554e1bfa8bbd9c514
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2020-10-02 20:59:37 +0900

    Update 1733.cpp

--- 1733.cpp
+++ 1733.cpp
@@ -1,17 +1,5 @@
-# pragma GCC optimize ("O3")
-# pragma GCC optimize ("Ofast")
-# pragma GCC optimize ("unroll-loops")
-
 #include <bits/stdc++.h>
-//#include <ext/pb_ds/assoc_container.hpp>
-//#include <ext/pb_ds/tree_policy.hpp>
-
-#pragma warning(disable:4996)
-#pragma comment(linker, "/STACK:336777216")
-
 using namespace std;
-//using namespace __gnu_pbds;
-
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
@@ -27,42 +15,38 @@ typedef tuple<ll, ll, ll, ll> tl4;
 typedef stack<ll> sl;
 typedef queue<ll> ql;
 typedef priority_queue<ll> pql;
-//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>ordered_set;
-
-const int dx[4] = { 1,-1,0,0 };
-const int dy[4] = { 0,0,1,-1 };
-const int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
-const int INF = 0x7f7f7f7f;
-const ll INF_LL = 0x7f7f7f7f7f7f7f7f;
-ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+ll MOD = 1000000000;
+ll POW(ll a, ll b, ll MMM = MOD) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
 ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
-ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a, b) * b; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a * (b / GCD(a, b)); }
 ll INV(ll a, ll m) {
-	ll m0 = m, y = 0, x = 1;
-	if (m == 1)	return 0;
-	while (a > 1) {
-		ll q = a / m;
-		ll t = m;
-		m = a % m, a = t;
-		t = y;
-		y = x - q * y;
-		x = t;
-	}
-	if (x < 0) x += m0;
-	return x;
+  ll m0 = m, y = 0, x = 1;
+  if (m == 1)	return 0;
+  while (a > 1) {
+    ll q = a / m;
+    ll t = m;
+    m = a % m, a = t;
+    t = y;
+    y = x - q * y;
+    x = t;
+  }
+  if (x < 0) x += m0;
+  return x;
 }
-pll EXGCD(ll a, ll b) {
-	if (b == 0) return { 1,0 };
-	auto t = EXGCD(b, a%b);
-	return { t.second,t.first - t.second*(a / b) };
+ll EXGCD(ll a, ll b, ll&x, ll&y) {
+  if (!b)return x = 1, y = 0, a;
+  ll d = EXGCD(b, a%b, x, y), t = x;
+  return x = y, y = t - a / b * y, d;
 }
 bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
 #define X first
 #define Y second
 #define rep(i,a,b) for(int i = a; i < b; i++)
-#define pb push_back
+#define pb(x) push_back(x)
 #define all(x) (x).begin(), (x).end()
-#define sz(a) ((int)(a.size()))
 #define sf1(a) cin >> a
 #define sf2(a,b) cin >> a >> b
 #define sf3(a,b,c) cin >> a >> b >> c
@@ -82,76 +66,100 @@ bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
 #define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
 #define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
 #define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
-#define pfvec(V) for(auto const &t : V) pf1(t)
-#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
-const int MX_A = 1000002;
-const int MX_B = 1000002;
-// 1-indexed
-vi A(MX_A, -1), B(MX_B, -1), level(MX_A); // A[i],B[i] : 매칭된 상대방의 정점, level[i] : i번째 정점의 depth
-vector<bool> used(MX_A); // used[i] : A_i가 매칭에 속해있는가?
-vi adj[MX_A]; // adj[i] : A_i와 연결된 B들의 index
-int lenA, lenB; // A의 갯수, B의 갯수
-void HF_bfs() {
-	qi Q;
-	rep(i, 1, lenA+1) {
-		if (!used[i]) {
-			level[i] = 0;
-			Q.push(i);
-		}
-		else level[i] = INF;
-	}
-	while (!Q.empty()) {
-		int a = Q.front();
-		Q.pop();
-		for (auto b : adj[a]) {
-			if (B[b] != -1 && level[B[b]] == INF) {
-				level[B[b]] = level[a] + 1;
-				Q.push(B[b]);
-			}
-		}
-	}
-}
-bool HF_dfs(int a) {
-	for (auto b : adj[a]) {
-		if (B[b] == -1 || level[B[b]] == level[a] + 1 && HF_dfs(B[b])) {
-			used[a] = true;
-			A[a] = b;
-			B[b] = a;
-			return true;
-		}
-	}
-	return false;
-}
-int HF() {
-	int match = 0;
-	while (true) {
-		HF_bfs();
-		int flow = 0;
-		rep(i, 1, lenA+1)
-			if (!used[i] && HF_dfs(i)) flow++;
-		if (flow == 0) break;
-		match += flow;
-	}
-	return match;
-}
+
+int n;
+vi occur[1000005];
+pii shirt[1000005];
+
+int choice[1000005];
+int isused[1000005];
+
+vector<pii> adj[1000005];
+pii p[1000005];
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int N;
-	sf1(N);
-	lenA = N;
-	lenB = 1000'000;
-	rep(i, 1, N + 1) {
-		int x, y;
-		sf2(x, y);
-		adj[i].pb(x);
-		adj[i].pb(y);
-	}
-	int match = HF();
-	if (match != N) {
-		pf1l(-1);
-		return 0;
-	}
-	rep(i, 1, N + 1)
-		pf1l(A[i]);
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  sf1(n);
+  qi fixed;
+  rep(i,0,n){
+    int a,b;
+    sf2(a,b);
+    if(a == b){
+      if(isused[a]){
+        pf1l(-1);
+        return 0;
+      }
+      fixed.push(a);
+      isused[a] = 1;
+      choice[i] = a;
+      shirt[i] = {a, a};
+    }
+    else{
+      occur[a].pb(i);
+      occur[b].pb(i);
+      shirt[i] = {a,b};
+    }
+  }
+  while(!fixed.empty()){
+    int cur = fixed.front(); fixed.pop();
+    for(auto v : occur[cur]){
+      if(choice[v] != 0) continue;
+      int other = shirt[v].X + shirt[v].Y - cur;
+      if(isused[other]){
+        pf1l(-1);
+        return 0;
+      }
+      isused[other] = 1;
+      choice[v] = other;
+      fixed.push(other);
+    }
+  }
+  rep(i,0,n){
+    if(choice[i] != 0) continue;
+    int a, b;
+    tie(a,b) = shirt[i];
+    adj[a].pb(make_pair(b, i));
+    adj[b].pb(make_pair(a, i));    
+  }
+  rep(i,1,1000001){
+    if(isused[i]) continue;
+    int left = -1; // tree에 속하지 않는 간선의 index
+    queue<int> q;
+    q.push(i);
+    isused[i] = 1;
+    while(!q.empty()){
+      int cur = q.front(); q.pop();
+      for(auto nxt : adj[cur]){  
+        if(isused[nxt.X]){
+          if(nxt.X == p[cur].X || left == nxt.Y) continue;
+          else{
+            if(left != -1){
+              pf1l(-1);
+//              assert(0);
+              return 0;
+            }
+            left = nxt.Y;
+            continue;
+          }
+        }
+        p[nxt.X] = {cur, nxt.Y};
+        choice[nxt.Y] = nxt.X;
+        q.push(nxt.X);
+        isused[nxt.X] = 1;
+      }
+    }
+    if(left != -1){
+      int a, b;
+      tie(a, b) = shirt[left];
+      choice[left] = a;
+      while(a != i){
+        choice[p[a].Y] = p[a].X;
+        a = p[a].X;
+      }
+    }
+  }
+  rep(i,0,n){
+    pf1l(choice[i]);
+    assert(choice[i] == shirt[i].X or choice[i] == shirt[i].Y);
+  }
 }
