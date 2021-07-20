776cf84f74251fff08768ad12d8d98626763b5a3
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-07-21 00:45:35 +0900

    Update 9466.cpp

--- 9466.cpp
+++ 9466.cpp
@@ -1,119 +1,71 @@
-# pragma GCC optimize ("O3")
-# pragma GCC optimize ("Ofast")
-# pragma GCC optimize ("unroll-loops")
-
 #include <bits/stdc++.h>
-
-#pragma warning(disable:4996)
-#pragma comment(linker, "/STACK:336777216")
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef tuple<int, int, int> ti3;
-typedef tuple<int, int, int, int> ti4;
-typedef stack<int> si;
-typedef queue<int> qi;
-typedef priority_queue<int> pqi;
-typedef pair<ll, ll> pll;
-typedef vector<ll> vl;
-typedef tuple<ll, ll, ll> tl3;
-typedef tuple<ll, ll, ll, ll> tl4;
-typedef stack<ll> sl;
-typedef queue<ll> ql;
-typedef priority_queue<ll> pql;
-const int dx[4] = { 1,-1,0,0 };
-const int dy[4] = { 0,0,1,-1 };
-const int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
-const int INF = 0x7f7f7f7f;
-const ll INF_LL = 0x7f7f7f7f7f7f7f7f;
-ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
-ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
-ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a, b) * b; }
-ll INV(ll a, ll m) {
-	ll m0 = m, y = 0, x = 1;
-	if (m == 1)	return 0;
-	while (a > 1) {
-		ll q = a / m;
-		ll t = m;
-		m = a % m, a = t;
-		t = y;
-		y = x - q * y;
-		x = t;
-	}
-	if (x < 0) x += m0;
-	return x;
+
+int arr[100005];
+int n;
+int state[100005];
+
+const int NOT_VISITED = 0;
+const int VISITED = 1;
+const int CYCLE_IN = 2;
+const int NOT_CYCLE_IN = 3;
+
+void run(int x){
+  int cur = x;
+  while(true){
+    state[cur] = VISITED;
+    cur = arr[cur];
+    // 1번 케이스
+    if(state[cur] == CYCLE_IN || state[cur] == NOT_CYCLE_IN){
+      cur = x;
+      while(state[cur] == VISITED){
+        state[cur] = NOT_CYCLE_IN;
+        cur = arr[cur];
+      }
+      return;
+    }
+    // 2번 케이스
+    if(state[cur] == VISITED && cur != x){
+      while(state[cur] != CYCLE_IN){
+        state[cur] = CYCLE_IN;
+        cur = arr[cur];
+      }
+      cur = x;
+      while(state[cur] != CYCLE_IN){
+        state[cur] = NOT_CYCLE_IN;
+        cur = arr[cur];
+      }
+      return;
+    }
+    // 3번 케이스
+    if(state[cur] == VISITED && cur == x){
+      while(state[cur] != CYCLE_IN){
+        state[cur] = CYCLE_IN;
+        cur = arr[cur];
+      }
+      return;
+    }
+  }
 }
-pll EXGCD(ll a, ll b) {
-	if (b == 0) return { 1,0 };
-	auto t = EXGCD(b, a%b);
-	return { t.second,t.first - t.second*(a / b) };
-}
-bool OOB(ll x, ll y, ll N, ll M) { return 0 > x || x >= N || 0 > y || y >= M; }
-#define X first
-#define Y second
-#define rep(i,a,b) for(int i = a; i < b; i++)
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define sz(a) ((int)(a.size()))
-#define sf1(a) cin >> a
-#define sf2(a,b) cin >> a >> b
-#define sf3(a,b,c) cin >> a >> b >> c
-#define sf4(a,b,c,d) cin >> a >> b >> c >> d
-#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
-#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
-#define pf1(a) cout << (a) << ' '
-#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
-#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
-#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
-#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
-#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
-#define pf0l() cout << '\n';
-#define pf1l(a) cout << (a) << '\n'
-#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
-#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
-#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
-#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
-#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
-#define pfvec(V) for(auto const &t : V) pf1(t)
-#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
-int N;
-int S[100005];
-vi state; // 0 : unused, -1 : used & team
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int T;
-	sf1(T);
-	while (T--) {
-		sf1(N);
-		rep(i, 1, N+1) sf1(S[i]);
-		state.clear();
-		state.resize(N + 1);
-		rep(i, 1, N + 1) {
-			if (state[i] != 0) continue;
-			int cur = i;
-			state[cur] = i;
-			while (true) {
-				cur = S[cur];
-				if (state[cur] == i) {
-					state[cur] = -1;
-					int tmp = S[cur];
-					while (tmp != cur) {
-						state[tmp] = -1;
-						tmp = S[tmp];
-					}
-					break;
-				}
-				if (state[cur] == 0) {
-					state[cur] = i;
-					continue;
-				}
-				else break;
-			}
-		}
-		int cnt = N;
-		rep(i, 1, N + 1) cnt -= (state[i] == -1);
-		pf1l(cnt);
-	}
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while(t--){
+    cin >> n;
+    fill(state+1, state+n+1, 0);
+    for(int i = 1; i <= n; i++)
+      cin >> arr[i];
+    int ans = 0;
+    for(int i = 1; i <= n; i++){
+      if(state[i] == NOT_VISITED) run(i);
+    }
+    int cnt = 0;
+    for(int i = 1; i <= n; i++){
+      if(state[i] == NOT_CYCLE_IN) cnt++;
+    }
+    cout << cnt << '\n';
+  }
 }
